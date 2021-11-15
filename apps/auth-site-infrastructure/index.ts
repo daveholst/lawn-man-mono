2496eb9ebba3ca473c0c057f89a889ad9e23f2e3
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-11-15 09:06:31 +0800

    auth-site now on CDN

--- apps/auth-site-infrastructure/index.ts
+++ apps/auth-site-infrastructure/index.ts
@@ -9,16 +9,26 @@ const domain = 'auth.lawnman.club'
 const domainParts = getDomainAndSubdomain(domain)
 const tenMinutes = 60 * 10
 
+// Per AWS, ACM certificate must be in the us-east-1 region.
+const eastRegion = new aws.Provider('east', {
+    profile: aws.config.profile,
+    region: 'us-east-1',
+})
+
 // Get the hosted zone by domain name
 const selectedZone = pulumi.output(
     aws.route53.getZone({ name: domainParts.parentDomain }, { async: true })
 )
 
 //  Create the SSL Certificate
-const certificate = new aws.acm.Certificate('auth-lawnman-certificate', {
-    domainName: domain,
-    validationMethod: 'DNS',
-})
+const certificate = new aws.acm.Certificate(
+    'auth-lawnman-certificate',
+    {
+        domainName: domain,
+        validationMethod: 'DNS',
+    },
+    { provider: eastRegion }
+)
 // Create the Certificate Validation Records in the DNS
 const certificateValidationDomain = new aws.route53.Record(
     'auth-lawnman-validation-record',
@@ -37,7 +47,8 @@ const certificateValidation = new aws.acm.CertificateValidation(
     {
         certificateArn: certificate.arn,
         validationRecordFqdns: [certificateValidationDomain.fqdn],
-    }
+    },
+    { provider: eastRegion }
 )
 
 // Create an AWS resource (S3 Bucket)
@@ -49,6 +60,78 @@ const siteBucket = new aws.s3.Bucket('auth-lawnman-bucket', {
     },
 })
 
+// distributionArgs configures the CloudFront distribution. Relevant documentation:
+// https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/distribution-web-values-specify.html
+// https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html
+const distributionArgs: aws.cloudfront.DistributionArgs = {
+    enabled: true,
+    // Alternate aliases the CloudFront distribution can be reached at, in addition to https://xxxx.cloudfront.net.
+    // Required if you want to access the distribution via config.targetDomain as well.
+    aliases: ['auth.lawnman.club'],
+
+    // We only specify one origin for this distribution, the S3 content bucket.
+    origins: [
+        {
+            originId: siteBucket.arn,
+            domainName: siteBucket.websiteEndpoint,
+            customOriginConfig: {
+                // Amazon S3 doesn't support HTTPS connections when using an S3 bucket configured as a website endpoint.
+                // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/distribution-web-values-specify.html#DownloadDistValuesOriginProtocolPolicy
+                originProtocolPolicy: 'http-only',
+                httpPort: 80,
+                httpsPort: 443,
+                originSslProtocols: ['TLSv1.2'],
+            },
+        },
+    ],
+
+    defaultRootObject: 'index.html',
+
+    // A CloudFront distribution can configure different cache behaviors based on the request path.
+    // Here we just specify a single, default cache behavior which is just read-only requests to S3.
+    defaultCacheBehavior: {
+        targetOriginId: siteBucket.arn,
+
+        viewerProtocolPolicy: 'redirect-to-https',
+        allowedMethods: ['GET', 'HEAD', 'OPTIONS'],
+        cachedMethods: ['GET', 'HEAD', 'OPTIONS'],
+
+        forwardedValues: {
+            cookies: { forward: 'none' },
+            queryString: false,
+        },
+
+        minTtl: 0,
+        defaultTtl: tenMinutes,
+        maxTtl: tenMinutes,
+    },
+
+    // "All" is the most broad distribution, and also the most expensive.
+    // "100" is the least broad, and also the least expensive.
+    priceClass: 'PriceClass_100',
+
+    // You can customize error responses. When CloudFront receives an error from the origin (e.g. S3 or some other
+    // web service) it can return a different error code, and return the response for a different resource.
+    customErrorResponses: [
+        { errorCode: 404, responseCode: 404, responsePagePath: '/404.html' },
+    ],
+
+    restrictions: {
+        geoRestriction: {
+            restrictionType: 'none',
+        },
+    },
+
+    viewerCertificate: {
+        acmCertificateArn: certificateValidation.certificateArn, // Per AWS, ACM certificate must be in the us-east-1 region.
+        sslSupportMethod: 'sni-only',
+    },
+}
+
+const cdn = new aws.cloudfront.Distribution(
+    'auth-lawnman-cdn',
+    distributionArgs
+)
 //  Create DNS record for bucket
 const staticDnsRecord = new aws.route53.Record(
     'apiDnsRecord',
@@ -60,15 +143,15 @@ const staticDnsRecord = new aws.route53.Record(
         aliases: [
             {
                 evaluateTargetHealth: true,
-                name: siteBucket.websiteDomain,
-                zoneId: siteBucket.hostedZoneId, //maybe just .id ???
+                // name: siteBucket.websiteDomain,
+                name: cdn.domainName,
+                // zoneId: siteBucket.hostedZoneId, //maybe just .id ???
+                zoneId: cdn.hostedZoneId, //maybe just .id ???
             },
         ],
     },
     { dependsOn: [certificateValidation, siteBucket] }
 )
-
-// upload files to the bucket --> moved to a build script
 
 // Export the name of the bucket
 export const bucketDomain = siteBucket.bucketDomainName
